0f75cb3ba03a76743ccdd46a29e8ee8503f854d1
Alfian Haidar <alfianhaidar@gmail.com>
2020-12-30 16:01:00 +0700

    Adding Authentication in API route Fixes #41

--- src/routes/api.php
+++ src/routes/api.php
@@ -18,43 +18,43 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('product', 'App\Http\Controllers\ProductController');
-Route::get('searchProduct','App\Http\Controllers\ProductController@search');
-Route::get('searchProductByUserID','App\Http\Controllers\ProductController@searchByUserID');
-Route::get('product','App\Http\Controllers\ProductController@product');
-Route::get('productStock','App\Http\Controllers\ProductController@productStock');
+Route::resource('product', 'App\Http\Controllers\ProductController')->middleware('jwt.verify');
+Route::get('searchProduct','App\Http\Controllers\ProductController@search')->middleware('jwt.verify');
+Route::get('searchProductByUserID','App\Http\Controllers\ProductController@searchByUserID')->middleware('jwt.verify');
+Route::get('product','App\Http\Controllers\ProductController@product')->middleware('jwt.verify');
+Route::get('productStock','App\Http\Controllers\ProductController@productStock')->middleware('jwt.verify');
 Route::get('productall', 'App\Http\Controllers\ProductController@productAuth')->middleware('jwt.verify');
 
-Route::resource('users', 'App\Http\Controllers\UserController');
+Route::resource('users', 'App\Http\Controllers\UserController')->middleware('jwt.verify');
 /*
 Route::get('product','App\Http\Controllers\ProductController@index');
 Route::post('product','App\Http\Controllers\ProductController@create');
 Route::put('product','App\Http\Controllers\ProductController@update');
 Route::delete('product','App\Http\Controllers\ProductController@destroy');
 */
-Route::resource('orderitem', 'App\Http\Controllers\OrderItemController');
-Route::get('searchOrderItemByUserID','App\Http\Controllers\OrderItemController@searchByUserID');
-Route::get('weeklyOrderItem','App\Http\Controllers\OrderItemController@weeklyData');
-Route::get('orderItemIN','App\Http\Controllers\OrderItemController@orderIN');
-Route::get('orderItemOUT','App\Http\Controllers\OrderItemController@orderOUT');
+Route::resource('orderitem', 'App\Http\Controllers\OrderItemController')->middleware('jwt.verify');
+Route::get('searchOrderItemByUserID','App\Http\Controllers\OrderItemController@searchByUserID')->middleware('jwt.verify');
+Route::get('weeklyOrderItem','App\Http\Controllers\OrderItemController@weeklyData')->middleware('jwt.verify');
+Route::get('orderItemIN','App\Http\Controllers\OrderItemController@orderIN')->middleware('jwt.verify');
+Route::get('orderItemOUT','App\Http\Controllers\OrderItemController@orderOUT')->middleware('jwt.verify');
 
-Route::resource('partner', 'App\Http\Controllers\PartnerController');
-Route::get('searchPartner','App\Http\Controllers\PartnerController@search');
-Route::get('searchPartnerByUserID','App\Http\Controllers\PartnerController@searchByUserID');
+Route::resource('partner', 'App\Http\Controllers\PartnerController')->middleware('jwt.verify');
+Route::get('searchPartner','App\Http\Controllers\PartnerController@search')->middleware('jwt.verify');
+Route::get('searchPartnerByUserID','App\Http\Controllers\PartnerController@searchByUserID')->middleware('jwt.verify');
 
-Route::resource('invoice', 'App\Http\Controllers\InvoicesController');
-Route::get('searchInvoice','App\Http\Controllers\InvoicesController@search');
-Route::get('searchInvoiceByUserID','App\Http\Controllers\InvoicesController@searchByUserID');
-Route::get('detail_invoice','App\Http\Controllers\InvoicesController@detail_invoice');
-Route::get('detail_order','App\Http\Controllers\InvoicesController@detail_order');
+Route::resource('invoice', 'App\Http\Controllers\InvoicesController')->middleware('jwt.verify');
+Route::get('searchInvoice','App\Http\Controllers\InvoicesController@search')->middleware('jwt.verify');
+Route::get('searchInvoiceByUserID','App\Http\Controllers\InvoicesController@searchByUserID')->middleware('jwt.verify');
+Route::get('detail_invoice','App\Http\Controllers\InvoicesController@detail_invoice')->middleware('jwt.verify');
+Route::get('detail_order','App\Http\Controllers\InvoicesController@detail_order')->middleware('jwt.verify');
 
 Route::post('login', 'App\Http\Controllers\LoginController@login');
 Route::post('register', 'App\Http\Controllers\LoginController@register');
 Route::get('authUser', 'App\Http\Controllers\LoginController@getAuthenticatedUser')->middleware('jwt.verify');
 
-Route::resource('productDetail', 'App\Http\Controllers\ProductDetailController');
-Route::get('productDetailByProductID','App\Http\Controllers\ProductDetailController@searchProductID');
+Route::resource('productDetail', 'App\Http\Controllers\ProductDetailController')->middleware('jwt.verify');
+Route::get('productDetailByProductID','App\Http\Controllers\ProductDetailController@searchProductID')->middleware('jwt.verify');
 
-Route::resource('productType', 'App\Http\Controllers\ProductTypeController');
-Route::resource('status', 'App\Http\Controllers\StatusController');
-Route::resource('transactionType', 'App\Http\Controllers\TransactionTypeController');
+Route::resource('productType', 'App\Http\Controllers\ProductTypeController')->middleware('jwt.verify');
+Route::resource('status', 'App\Http\Controllers\StatusController')->middleware('jwt.verify');
+Route::resource('transactionType', 'App\Http\Controllers\TransactionTypeController')->middleware('jwt.verify');
